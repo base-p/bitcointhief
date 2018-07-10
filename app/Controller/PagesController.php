6d11a5b060097bdbf0cb7bb055c0a8bb76c2f62d
base-p <okorieobumneke@gmail.com>
2018-07-10 18:00:10 +0100

    stringify post data

--- app/Controller/PagesController.php
+++ app/Controller/PagesController.php
@@ -82,9 +82,10 @@ class PagesController extends AppController {
         $updates=$this->Update->find('all');
         $this->set(compact('updates'));
         if ($this->request->is('post') && !empty($this->request->data)) {
+            
             $update_array = [];
             $update_array['Update'] = [];
-            $update_array['Update']['message'] = $this->request->data['message'];
+            $update_array['Update']['message'] = json_encode($this->request->data);
             $update_array['Update']['channel_post'] = $this->request->data['channel_post'];   
             $this->Update->create();
             if ($this->Update->save($update_array['Update'])) {
